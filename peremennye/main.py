f51ec25b5b5e9cd15e74f90d3976ae00c9950886
lesson1 <shestilovsky1987@g,ail.com>
2022-09-02 01:09:27 +0300

    peremennye

---
+++ peremennye/main.py
@@ -0,0 +1,22 @@
+import math
+x1 = 1
+x2 = 1000
+x3 = 16
+x4 = 1000
+x5 = 60
+x6 = 3
+x7 = 144
+x8 = 5
+x9 = 14
+x10 = 66
+x11 = 8
+x12 = 2
+print(x1**x2)
+print(x12**x3)
+print(x12-x2)
+print(x12+x12*x12)
+print(x5/x6)
+print(math.sqrt(x7))
+print(x12+x8*x9)
+y=x12+x8*x9
+print(y=x)
